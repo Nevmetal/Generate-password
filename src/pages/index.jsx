a8a33854e5d2d37ed5ef08b34987bc28bbf00ff8
Nevmetal <wbgua@live.com>
2024-03-22 10:47:21 -0500

    add first version ramdon password

---
+++ src/pages/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import backstyle from "../styles/background.module.css"
+export default function PasswordGenerator() {
+  const [password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(12);
+
+  const handleInputChange = (event) => {
+    const length = parseInt(event.target.value);
+    if (!isNaN(length)) {
+      setPasswordLength(length);
+    }
+  };
+
+  const generatePassword = () => {
+    const charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let newPassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      newPassword += charset[randomIndex];
+    }
+    setPassword(newPassword);
+  };
+
+  return (
+    <main className={`${backstyle.cssselector} h-screen grid content-center `}>
+      <article className="m-auto mt-2  max-w-3xl p-6 rounded-lg bg-gray-800 border-gray-700 shadow-violet-500 shadow-2xl">
+        <div className="flex content-center">
+          <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight md:text-4xl lg:text-5xl text-white text-center ">
+            Generador de Contraseñas
+          </h1>
+        </div>
+
+        <label className="text-2xl italic">Longitud de la contraseña:</label>
+        <div className="grid gap-6 grid-cols-3 content-center p-2 mb-2 ">
+          <input
+            className="col-span-2 rounded-lg cursor-pointer bg-gray-700"
+            type="range"
+            defaultValue={passwordLength}
+            onChange={handleInputChange}
+            min="1"
+            max="128"
+          />
+          <span>{passwordLength}</span>
+        </div>
+
+        <textarea
+          className="block p-3 w-full text-sm md:h-20 bg-gray-50 rounded-lg border  dark:bg-gray-700 border-gray-600  text-white text-balance "
+          defaultValue={password}
+          disabled
+        ></textarea>
+        <button
+          className="text-white mt-2 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800"
+          onClick={generatePassword}
+        >
+          Generar Contraseña
+        </button>
+      </article>
+    </main>
+  );
+}
