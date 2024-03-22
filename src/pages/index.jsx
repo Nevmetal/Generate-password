a4f86dcae28bbc26ec6bb326216ebe60a0c7069f
Nevmetal <wbgua@live.com>
2024-03-22 11:06:25 -0500

    Add character check selector

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
-import backstyle from "../styles/background.module.css"
+import backstyle from "../styles/background.module.css";
 export default function PasswordGenerator() {
   const [password, setPassword] = useState("");
   const [passwordLength, setPasswordLength] = useState(12);
-
+  const [options, setOptions] = useState({
+    includeNumbers: true,
+    includeLetters: true,
+    includeSpecialChars: true,
+  });
   const handleInputChange = (event) => {
     const length = parseInt(event.target.value);
     if (!isNaN(length)) {
       setPasswordLength(length);
     }
   };
+  const toggleOption = (option) => {
+    setOptions({ ...options, [option]: !options[option] });
+  };
 
   const generatePassword = () => {
-    const charset =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    const charset = generateCharset();
+    if (charset.length === 0) {
+      alert("Es requerido incluir como mínimo un tipo de caracter para generar la contraseña");
+      return;
+    }
     let newPassword = "";
     for (let i = 0; i < passwordLength; i++) {
       const randomIndex = Math.floor(Math.random() * charset.length);
@@ -21,7 +31,19 @@ export default function PasswordGenerator() {
     }
     setPassword(newPassword);
   };
-
+  const generateCharset = () => {
+    let charset = "";
+    if (options.includeLetters) {
+      charset += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (options.includeNumbers) {
+      charset += "0123456789";
+    }
+    if (options.includeSpecialChars) {
+      charset += "!@#$%^&*()_+";
+    }
+    return charset;
+  };
   return (
     <main className={`${backstyle.cssselector} h-screen grid content-center `}>
       <article className="m-auto mt-2  max-w-3xl p-6 rounded-lg bg-gray-800 border-gray-700 shadow-violet-500 shadow-2xl">
@@ -43,7 +65,37 @@ export default function PasswordGenerator() {
           />
           <span>{passwordLength}</span>
         </div>
+        <div>
+          <input
+            type="checkbox"
+            id="includeNumbers"
+            checked={options.includeNumbers}
+            onChange={() => toggleOption("includeNumbers")}
+          />
+          <label htmlFor="includeNumbers">Incluir números</label>
+        </div>
 
+        <div>
+          <input
+            type="checkbox"
+            id="includeLetters"
+            checked={options.includeLetters}
+            onChange={() => toggleOption("includeLetters")}
+          />
+          <label htmlFor="includeLetters">Incluir letras</label>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            id="includeSpecialChars"
+            checked={options.includeSpecialChars}
+            onChange={() => toggleOption("includeSpecialChars")}
+          />
+          <label htmlFor="includeSpecialChars">
+            Incluir caracteres especiales
+          </label>
+        </div>
         <textarea
           className="block p-3 w-full text-sm md:h-20 bg-gray-50 rounded-lg border  dark:bg-gray-700 border-gray-600  text-white text-balance "
           defaultValue={password}
